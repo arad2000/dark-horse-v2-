bf92a7d39d9408d2f3be7e33dc81c581d483baa4
arad2000 <arad2000ho@gmail.com>
2026-09-10 09:49:49 +0330

    fix: restore escapeHtml quote escaping

--- docs/app.js
+++ docs/app.js
@@ -51,7 +51,7 @@ function escapeHtml(text) {
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
-    .replace(/\"/g, '&quot;')
+    .replace(/"/g, '&quot;')
     .replace(/'/g, '&#039;');
 }
 
