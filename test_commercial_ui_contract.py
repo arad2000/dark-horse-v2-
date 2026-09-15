cfaeeb16400adeb32209791bab31de8d1f60c73b
arad2000 <arad2000ho@gmail.com>
2026-09-15 20:34:01 +0330

    test: accept cache-busted commercial UI script versions

--- test_commercial_ui_contract.py
+++ test_commercial_ui_contract.py
@@ -28,8 +28,8 @@ class CommercialUIContractTests(unittest.TestCase):
             self.assertIn(marker, self.auth)
 
     def test_commercial_ui_is_loaded_after_auth_client(self):
-        auth_pos = self.index.index('src="auth_api_client.js"')
-        ui_pos = self.index.index('src="commercial_ui.js?v=1"')
+        auth_pos = self.index.index('src="auth_api_client.js?v=2"')
+        ui_pos = self.index.index('src="commercial_ui.js?v=2"')
         self.assertLess(auth_pos, ui_pos)
         self.assertIn("DHAuth.createPayment", self.ui)
         self.assertIn("DHAuth.consumeTest", self.ui)
