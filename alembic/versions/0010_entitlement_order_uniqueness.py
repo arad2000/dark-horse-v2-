86f29c1aa499455b52d1e5c5aa48d255f23abbff
arad2000 <arad2000ho@gmail.com>
2026-09-18 07:02:44 +0330

    fix: shorten Alembic revision id to 32 chars

--- alembic/versions/0010_entitlement_order_uniqueness.py
+++ alembic/versions/0010_entitlement_order_uniqueness.py
@@ -2,7 +2,7 @@
 from alembic import op
 import sqlalchemy as sa
 
-revision = "0010_entitlement_order_uniqueness"
+revision = "0010_entitlement_order_unique"
 down_revision = "0009_journey_credit_consumptions"
 branch_labels = None
 depends_on = None
